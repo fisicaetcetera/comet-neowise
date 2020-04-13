fe89c403f4f6c480040376805f33d04392acaa79
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-04-13 19:10:35 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+//Nessa versão, vou colocar minha imagem como fundo
+//let cam;
+
+let video;
+
+let earthjpg;
+let moonjpg;
+let sunjpg;
+let assinatura;
+let posx=-200, posz=100;
+
+function preload() {
+  earthjpg = loadImage('earthcloud-1.jpg');
+  moonjpg = loadImage('moonmap1k.jpg');
+  sunjpg = loadImage('sun-1.jpg');
+  starsjpg = loadImage('stars.jpg');
+}
+
+function setup() {
+  createCanvas(640, 480, WEBGL);
+
+  video = createCapture(VIDEO);
+  video.hide();
+  
+  assinatura = createGraphics(380, 100);
+  assinatura.background(255, 100);
+  assinatura.fill(0);
+  assinatura.textAlign(CENTER);
+  assinatura.textSize(90);
+  assinatura.text('Bonelli', 190, 85);
+
+}
+
+function draw() {
+  background(0);
+
+  //translate(0,0,mouseX);
+  push();
+  translate(0, 0, -1000);
+  scale(-1.0,1.0);
+  texture(video);
+  plane(2500);
+  pop();
+
+  push();
+  texture(assinatura);
+  translate(posx++,50,posz--);
+  rotateY(frameCount/95);
+  rotateX(frameCount/130);
+  plane(50, 30);
+  pop();
+  //directionalLight(255, 255, 255, 1, 0, 0)
+  //pointLight(255,255,0,0,0,200);
+
+  push();
+
+  texture(earthjpg);
+  rotateY(frameCount / 3280);
+  sphere(80);
+  
+  rotateY(frameCount / 1450);
+  translate(0, 0, -200);
+  texture(moonjpg);
+  sphere(12);
+  
+  pop();
+  
+  push();
+  rotateY(frameCount / 1500);
+  translate(0, 0, -1000);
+  texture(sunjpg);
+  sphere(25);
+  pop();
+  
+}
